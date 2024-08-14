109f678d7903cbff9cfb67877d1aca1a2ec810ea
safaminhaj <safaminhaj11@gmail.com>
2024-08-14 17:29:44 +0530

    pagination implemented

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -1,5 +1,61 @@
+import { useLoaderData, useLocation, useNavigate } from "react-router-dom";
+
 const PaginationContainer = () => {
-  return <div>PaginationContainer</div>;
+  const { meta } = useLoaderData();
+  const { page, pageCount } = meta.pagination;
+  const pages = Array.from({ length: pageCount }, (_, index) => {
+    return index + 1;
+  });
+
+  const { search, pathname } = useLocation();
+  const navigate = useNavigate();
+  const handlePageChange = (pageNumber) => {
+    const searchParams = new URLSearchParams(search);
+    searchParams.set("page", pageNumber);
+    navigate(`${pathname}?${searchParams.toString()}`);
+  };
+
+  if (pageCount < 2) return null;
+
+  return (
+    <div className="mt-16 flex justify-end">
+      <div className="join">
+        <button
+          className="join-item btn btn-xs sm:btn-md"
+          onClick={() => {
+            let prevPage = page - 1;
+            if (prevPage < 1) prevPage = pageCount;
+            handlePageChange(prevPage);
+          }}
+        >
+          Prev
+        </button>
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              key={pageNumber}
+              onClick={() => {
+                let nextPage = page + 1;
+                if (nextPage > pageCount) nextPage = 1;
+                handlePageChange(pageNumber);
+              }}
+              className={`join-item btn btn-xs sm:btn-md ${
+                pageNumber === page ? "bg-base-300 border-base-300" : ""
+              }`}
+            >
+              {pageNumber}
+            </button>
+          );
+        })}
+        <button
+          className="join-item btn btn-xs sm:btn-md"
+          onClick={() => handlePageChange("next")}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default PaginationContainer;
